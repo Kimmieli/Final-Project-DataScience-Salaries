9aaa40db120f01b105754a54f10641d4e1d727be
Lien Lai <lien0hao@gmail.com>
2022-03-21 22:45:06 -0400

    Update Final Project in R.R

--- Final Project in R.R
+++ Final Project in R.R
@@ -10,13 +10,13 @@ library(treemap)
 
 
 
-Salary2 = subset(Data.Science.Salaries._cleaned.2021, select = -c(1:23, 40:42))
+Salary2 = subset(Data.Fram.in.Excel.Data.Science.Salaries.2021, select = -c(1:23, 40:42))
 salary3 <- as.matrix(Salary2)
 heatmap(salary3)
 
-treemap(Data.Science.Salaries._cleaned.2021, index=c("Type.of.ownership"), vSize="Avg.Salary.K.", type="index")
-treemap(Data.Science.Salaries._cleaned.2021, index=c("Sector"), vSize = "Lower.Salary", type = "index")
-treemap(Data.Science.Salaries._cleaned.2021, index=c("Sector"), vSize = "Upper.Salary", type = "index")
+treemap(Data.Fram.in.Excel.Data.Science.Salaries.2021, index=c("Type.of.ownership"), vSize="Avg.Salary.K.", type="index")
+treemap(Data.Fram.in.Excel.Data.Science.Salaries.2021, index=c("Sector"), vSize = "Lower.Salary", type = "index")
+treemap(Data.Fram.in.Excel.Data.Science.Salaries.2021, index=c("Sector"), vSize = "Upper.Salary", type = "index")
 
 
 Data_Science_Salaries_cleaned_2021$NewSector <- NA
@@ -47,33 +47,33 @@ Data_Science_Salaries_cleaned_2021$NewSector[Data_Science_Salaries_cleaned_2021$
 Data_Science_Salaries_cleaned_2021$NewSector[Data_Science_Salaries_cleaned_2021$NewSector=='Accounting & Legal'] <- 25
 
 library(dplyr)
-Data_Science_Salaries_cleaned_2021 <- subset (Data_Science_Salaries_cleaned_2021, select = -NewSector)
+Data.Fram.in.Excel.Data.Science.Salaries.2021 <- subset (Data.Fram.in.Excel.Data.Science.Salaries.2021, select = -NewSector)
 
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Information Technology'] <- "1"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Biotech & Pharmaceuticals'] <- "2"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Business Services']<- "3"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Insurance'] <- "4"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Health Care'] <- "5"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Finance'] <- "6"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Manufacturing'] <- "7"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Aerospace & Defense'] <- "8"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Education'] <- "9"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Retail'] <- "10"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Oil, Gas, Energy & Utilities'] <- "11"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Government'] <- "12"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='-1'] <- "13"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Non-Profit'] <- "14"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Transportation & Logistics'] <- "15"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Real Estate'] <- "16"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Travel & Tourism'] <- "17"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Telecommunications'] <- "18"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Media'] <- "19"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Arts, Entertainment & Recreation'] <- "20"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Consumer'] <- "21"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Mining & Metals'] <- "22"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Construction, Repair & Maintenance'] <- "23"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Agriculture & Forestry'] <- "24"
-Data.Science.Salaries._cleaned.2021$NewSector[Data.Science.Salaries._cleaned.2021$Sector=='Accounting & Legal'] <- "25"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Information Technology'] <- "1"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Biotech & Pharmaceuticals'] <- "2"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Business Services']<- "3"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Insurance'] <- "4"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Health Care'] <- "5"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Finance'] <- "6"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Manufacturing'] <- "7"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Aerospace & Defense'] <- "8"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Education'] <- "9"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Retail'] <- "10"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Oil, Gas, Energy & Utilities'] <- "11"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Government'] <- "12"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='-1'] <- "13"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Non-Profit'] <- "14"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Transportation & Logistics'] <- "15"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Real Estate'] <- "16"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Travel & Tourism'] <- "17"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Telecommunications'] <- "18"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Media'] <- "19"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Arts, Entertainment & Recreation'] <- "20"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Consumer'] <- "21"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Mining & Metals'] <- "22"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Construction, Repair & Maintenance'] <- "23"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Agriculture & Forestry'] <- "24"
+Data.Fram.in.Excel.Data.Science.Salaries.2021$NewSector[Data.Fram.in.Excel.Data.Science.Salaries.2021$Sector=='Accounting & Legal'] <- "25"
 
 
 library("caret")
@@ -83,12 +83,12 @@ library("tidyr")
 library("lmtest")
 library("popbio")
 library("e1071")
-salarieslogit <- glm(NewSector ~ Python, data = Data.Science.Salaries._cleaned.2021, family = "binomial")
+salarieslogit <- glm(as.factor(NewSector) ~ Python, data = Data.Fram.in.Excel.Data.Science.Salaries.2021, family = "binomial")
 probabilities <- predict(salarieslogit, type = "response")
 
 install.packages("qpcR")
 library("qpcR")
-bindSalaries <- qpcR:::cbind.na(Data.Science.Salaries._cleaned.2021, probabilities)
+bindSalaries <- qpcR:::cbind.na(Data.Fram.in.Excel.Data.Science.Salaries.2021, probabilities)
 bindSalaries
 newBindSalaries <- na.omit(bindSalaries)
 newBindSalaries$Predicted <- ifelse(probabilities > .5, "yes", "no")
